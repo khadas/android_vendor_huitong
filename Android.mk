962498f98b5ad2fa803421935ecb07eba3a95171
Kuibao Zhang <kuibao.zhang@amlogic.com>
2017-08-23 19:17:08 +0800

    BT: add huitong bt remote audio function[4/4]
    PD#147865
    
    add huitong voice library
    
    Change-Id: Ie8cc0e06dc8893ede930dee5345400ba8220120a

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := audio.huitong.default.so
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := huitong
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/hw
+include $(BUILD_PREBUILT)
+
